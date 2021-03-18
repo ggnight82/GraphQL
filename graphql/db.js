fdd6eda1b7cfcbab65545bc41c1be19edbbb6350
ggnight82 <ggnight82@naver.com>
2021-03-18 20:39:52 +0900

    Query with argumnets

--- graphql/db.js
+++ graphql/db.js
@@ -1,42 +1,42 @@
 export const people = [
     {
-        id : "0",
+        id : 0,
         name : "Jungwoo0",
         age : 26,
         gender : "male"
     },
     {
-        id : "1",
+        id : 1,
         name : "Jungwoo1",
         age : 26,
         gender : "female"
     },
     {
-        id : "2",
+        id : 2,
         name : "Jungwoo2",
         age : 26,
         gender : "male"
     },
     {
-        id : "3",
+        id : 3,
         name : "Jungwoo3",
         age : 26,
         gender : "female"
     },
     {
-        id : "4",
+        id : 4,
         name : "Jungwoo4",
         age : 26,
         gender : "male"
     },
     {
-        id : "5",
+        id : 5,
         name : "Jungwoo5",
         age : 26,
         gender : "female"
     },
     {
-        id : "6",
+        id : 6,
         name : "Jungwoo6",
         age : 26,
         gender : "male"
@@ -46,6 +46,6 @@ export const people = [
 ];
 
 export const getById = id => {
-    const filteredPeople = people.filter(person => id === people.id);
+    const filteredPeople = people.filter(person => id === person.id);
     return filteredPeople[0];
 }
