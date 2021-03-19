71856498224460b9c660205c1a10efafa0e7ebfa
ggnight82 <ggnight82@naver.com>
2021-03-19 18:54:56 +0900

    Mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,51 +1,41 @@
-export const people = [
+let movies = [
     {
         id : 0,
-        name : "Jungwoo0",
-        age : 26,
-        gender : "male"
+        name : "Movie",
+        score : 1
     },
     {
         id : 1,
-        name : "Jungwoo1",
-        age : 26,
-        gender : "female"
+        name : "Avengers",
+        score : 8
     },
     {
         id : 2,
-        name : "Jungwoo2",
-        age : 26,
-        gender : "male"
+        name : "The godfather",
+        score : 99
     },
     {
         id : 3,
-        name : "Jungwoo3",
-        age : 26,
-        gender : "female"
+        name : "Logan",
+        score : 2
     },
-    {
-        id : 4,
-        name : "Jungwoo4",
-        age : 26,
-        gender : "male"
-    },
-    {
-        id : 5,
-        name : "Jungwoo5",
-        age : 26,
-        gender : "female"
-    },
-    {
-        id : 6,
-        name : "Jungwoo6",
-        age : 26,
-        gender : "male"
-    }
     
 
 ];
 
-export const getById = id => {
-    const filteredPeople = people.filter(person => id === person.id);
-    return filteredPeople[0];
+export const getMovies = () => movies;
+
+export const getById = (id) => {
+    const filteredMovies = movies.filter(movie => id === movie.id);
+    return filteredMovies[0];
+};
+
+export const deleteMovie = (id) => {
+    const cleanedMovies = movies.filter(movie => id !== movie.id);
+     if(movies.length > cleanedMovies.length){
+        movie = cleanedMovies;
+        return true;
+     } else {
+        return false;
+     }
 }
